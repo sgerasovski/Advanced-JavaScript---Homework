6207c2a0938ebfada41967e76f0b5cc34cb228fb
Slavcho Gerasovski <you@example.com>
2020-02-13 10:51:11 +0100

    Homework No.3 - Fix

--- aJS_08/main.js
+++ aJS_08/main.js
@@ -3,29 +3,19 @@ let btn = $("#search");
 btn.click(() => {
     let input = $("#country").val();
 
-    fetch(`http://restcountries.eu/rest/v2/name/${input}`)
+    fetch(`https://restcountries.eu/rest/v2/alpha/${input}`)
         .then(res => res.json())
         .then(data => {
             console.log("Country:")
-            console.log(data[0].name)
+            console.log(data.name)
             console.log("Neighbours:")
 
-            let border = data[0].borders
+            let border = data.borders
             border.forEach(element => {
-                fetch(`http://restcountries.eu/rest/v2/name/${element}`)
-                    .then(res => {
-                        if (res.status === 404) {
-                            fetch(`http://restcountries.eu/rest/v2/name/${element.substring(0, element.length - 1)}`)
-                                .then(res => res.json())
-                                .then(data => {
-                                    console.log(data[0].name)
-                                })
-                        } else {
-                            return res.json()
-                        }
-                    })
+                fetch(`https://restcountries.eu/rest/v2/alpha/${element}`)
+                    .then(res => res.json())
                     .then(data => {
-                        console.log(data[0].name)
+                        console.log(data.name)
                     })
             });
         })
